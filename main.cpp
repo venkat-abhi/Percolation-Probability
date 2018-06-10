4c2a83e8d148f401536ece985f9bf1aaf86a3691
K Venkata Abhishek <venkat.abhishek.1999@gmail.com>
2018-06-10 10:56:55 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+//#include<ctime>
+#include<time.h>
+#include"percolation_stats.h"
+using namespace std;
+
+int main() {
+	srand((int)time(0));
+	Percolation test;
+	test.stats();
+	while (true);
+	return 0;
+}
---
+++ percolation_stats.h
@@ -0,0 +1,27 @@
+#ifndef Percolation_h
+#define Percolation_h
+
+class Percolation {
+private:
+	int size;
+	int **array;
+	int top, bottom;
+	float probability;
+	float count;
+	float randrun;
+public:
+    Percolation(){
+	    top = 2;
+	    bottom = 11;
+    }
+	int stats();                                       //Checks for percolation 1000 times. 
+	int matrixdetails();
+	int percolates();
+	int matrixgen();
+	int getprobability();
+	int open();
+	int cluster();
+	int viewMatrix();
+};
+
+#endif
